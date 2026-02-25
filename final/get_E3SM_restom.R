d64f858fa88f9bab91d740d14c348b8966d85549
dyarger <60297933+dyarger@users.noreply.github.com>
2026-02-25 10:06:10 -0500

    Update get_E3SM_restom.R

--- final/get_E3SM_restom.R
+++ final/get_E3SM_restom.R
@@ -1,6 +1,6 @@
 
 
-setwd('~/Documents/Autotuning-NGD/')
+setwd('Autotuning-NGD/')
 files <- list.files('data')
 library(ncdf4)
 restom_file <- nc_open('data/lat_lon_10yr_180x360_ANN.nc')
@@ -23,7 +23,7 @@ PRECL <- ncvar_get(restom_file, 'PRECL')
 PRECC <-  apply(area_norm *PRECC,3, sum)
 PRECL <-  apply(area_norm *PRECL,3, sum)
 
-save(E3SM_RESTOM, PRECC, PRECL, e3sm_parameters, file = '~/Downloads/E3SM_RESTOM.RData')
+save(E3SM_RESTOM, PRECC, PRECL, e3sm_parameters, file = 'data/E3SM_RESTOM.RData')
 
 
 
